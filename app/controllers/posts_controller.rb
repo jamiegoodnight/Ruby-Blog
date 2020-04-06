52aa8fc5bd3027715c85af44d4fa6b86b480d021
Jamie Goodnight <jamiegoodnight3@gmail.com>
2020-04-05 22:13:04 -0400

    :construction: Building out posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
-    
+
     def index
+    end
+
+    def new 
+        @post = Post.new
+    end 
+
+    def create 
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+    end 
+
+    private
+
+    def post_params
+        params.required(:post).permit(:title, :content)
     end
 
 end
